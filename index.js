fd36efcc8ead07c550bf49058499bd8f86ad4db3
mvuas10 <mvuas10@gmail.com>
2020-03-31 16:45:01 +0200

    try 1 - new joke object

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
 const express = require('express')
 const app = express()
 
-const page = 
-`<html>
-    <head>
-        <title>Minh Nhat Vu</title>
-    </head>
+const jokes = {
+    "joke_1": {"What do you call a boommeran that doesn't work?": "A stick"},
+    "joke_2": {"The same bike tries to run me down every day": "Sounds like a vicious cycle"},
+    "joke_3": {"What do you call a guy who's had too much to drink?": "A cab"},
+    "joke_4": {"What kinds of tree has a hand?": "A palm tree"},
+    "joke_5": {"How does the solor system organize a party?": "They planet!"}
+}
 
-    <body>
-        <h1>Why do comedians love !false?</h1>
-        <p>It's funny because it's true.</p>
-    </body>
-</html>`
+function render (joke) {
+    const page = 
+            `<html>
+                <head>
+                    <title>Jokes</title>
+                </head>
 
-app.get('/', (req, res) => res.send(page))
+                <body>
+                    <h1>${jokes.joke_1}</h1>
+                </body>
+            </html>`
+    return page
+}
+
+
+app.get(
+    '/user/age', 
+    (req, res) => {
+        const joke = `${req.params.age}`
+        const laugh = render (joke)
+    }
+    
+    res.send(laugh))
 
 // use $PORT if it is defined
 // use 3000 if $PORT is not defined
